0112170cda00ebbf24031143ed13cca36976bdee
Andy Samaniego <spaceseeker6@hotmail.com>
2017-08-28 23:52:40 -0400

    Initial Forge Mod files

---
+++ src/main/java/andy/tutorialmod/Reference.java
@@ -0,0 +1,11 @@
+package andy.tutorialmod;
+
+public class Reference 
+{
+	public static final String MODID = "tm";
+	public static final String NAME = "Tutorial Mod";
+	public static final String VERSION = "0.1 - MC 1.12.1";
+	
+	public static final String CLIENTPROXY = "andy.tutorialmod.proxy.ClientProxy";
+	public static final String COMMONPROXY = "andy.tutorialmod.proxy.CommonProxy";
+}
---
+++ src/main/java/andy/tutorialmod/handlers/RegistryHandler.java
@@ -0,0 +1,14 @@
+package andy.tutorialmod.handlers;
+
+public class RegistryHandler 
+{
+	public static void Client()
+	{
+		
+	}
+	
+	public static void Common()
+	{
+		
+	}
+}
